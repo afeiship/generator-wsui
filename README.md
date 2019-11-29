d8386b78fa344c4bd048dbf4174e2e5a206e3779
afeiship <1290657123@qq.com>
2019-11-29 19:29:28 +0800

    fix: eslint err

--- README.md
+++ README.md
@@ -1,36 +1,20 @@
-# generator-webkit-sassui [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+# generator-webkit-sassui
 > Generator for webkit sass ui module.
 
-## Installation
-
-First, install [Yeoman](http://yeoman.io) and generator-webkit-sassui using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
-
+## installation
 ```bash
 npm install -g yo
 npm install -g generator-webkit-sassui
+
+
+# private install
+git clone git@github.com:afeiship/generator-webkit-sassui.git
+npm install 
+npm link
 ```
 
-Then generate your new project:
+## new project:
 
 ```bash
 yo webkit-sassui
 ```
-
-## Getting To Know Yeoman
-
- * Yeoman has a heart of gold.
- * Yeoman is a person with feelings and opinions, but is very easy to work with.
- * Yeoman can be too opinionated at times but is easily convinced not to be.
- * Feel free to [learn more about Yeoman](http://yeoman.io/).
-
-## License
-
-MIT © [feizheng](https://github.com/afeiship)
-
-
-[npm-image]: https://badge.fury.io/js/generator-webkit-sassui.svg
-[npm-url]: https://npmjs.org/package/generator-webkit-sassui
-[travis-image]: https://travis-ci.org/afeiship/generator-webkit-sassui.svg?branch=master
-[travis-url]: https://travis-ci.org/afeiship/generator-webkit-sassui
-[daviddm-image]: https://david-dm.org/afeiship/generator-webkit-sassui.svg?theme=shields.io
-[daviddm-url]: https://david-dm.org/afeiship/generator-webkit-sassui
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,69 +1,74 @@
-'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
-var yoHelper = require('yeoman-generator-helper');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const glob = require("glob");
+const { resolve } = require("path");
+const remote = require("yeoman-remote");
+const yoHelper = require("@feizheng/yeoman-generator-helper");
+const replace = require("replace-in-file");
 
-
-module.exports = yeoman.Base.extend({
-  prompting: function() {
+module.exports = class extends Generator {
+  prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the bedazzling ' + chalk.red('generator-webkit-cssui') + ' generator!'
-    ));
-
-    var prompts = [{
-      type: 'input',
-      name: 'project_name',
-      message: 'Your project name?',
-      default: yoHelper.discoverRoot
-    }, {
-      type: 'input',
-      name: 'description',
-      message: 'Description?'
-    }];
-
-    return this.prompt(prompts).then(function(props) {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    }.bind(this));
-  },
-
-  writing: function() {
-    yoHelper.rewriteProps(this.props);
-    this._rewriteShortName();
-    this._writingCopyFiles();
-    this._writingTplFiles();
-    this._writingTemplate();
-  },
-  _rewriteShortName: function() {
-    var projectNames = this.props.project_name.split('-');
-    this.props.project_short_name = projectNames.slice(2).join('-');
-  },
-  _writingCopyFiles: function() {
-    this.fs.copy(
-      this.templatePath('{.*,build/*}'),
-      this.destinationPath('.')
+    this.log(
+      yosay(
+        `Welcome to the stunning ${chalk.red(
+          "generator-webkit-sassui"
+        )} generator!`
+      )
     );
-  },
-  _writingTplFiles: function() {
-    this.fs.copyTpl(
-      this.templatePath('{*,docs/*}'),
-      this.destinationPath('.'),
-      this.props
+
+    const prompts = [
+      {
+        type: "input",
+        name: "project_name",
+        message: "Your project_name (eg: like this `react-button` )?",
+        default: yoHelper.discoverRoot
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Your description?"
+      }
+    ];
+
+    return this.prompt(prompts).then(
+      function(props) {
+        // To access props later use this.props.someAnswer;
+        this.props = props;
+        yoHelper.rewriteProps(props);
+      }.bind(this)
     );
-  },
-  _writingTemplate: function() {
-    this.fs.copyTpl(
-      this.templatePath('src/template.scss'),
-      this.destinationPath('src/' + this.props.project_name + '.scss'),
-      this.props
-    );
-  },
-  install: function() {
-    console.log('install dependencies....');
-  },
-  end: function() {
-    console.log('Enjoy coding~ :)');
   }
-});
+
+  writing() {
+    const done = this.async();
+    remote(
+      "afeiship",
+      "boilerplate-webkit-sassui",
+      function(err, cachePath) {
+        // Copy files:
+        this.fs.copy(
+          glob.sync(resolve(cachePath, "{**,.*}")),
+          this.destinationPath()
+        );
+        done();
+      }.bind(this)
+    );
+  }
+
+  end() {
+    const { project_name, description } = this.props;
+    const files = glob.sync(resolve(this.destinationPath(), "{**,.*}"));
+
+    replace.sync({
+      files,
+      from: [
+        /boilerplate-webkit-sassui-description/g,
+        /boilerplate-webkit-sassui/g
+      ],
+      to: [description, project_name]
+    });
+  }
+};
